582281486eeb1708923d4b72027cacf068259763
TihimVecherom <98401044+TihimVecherom@users.noreply.github.com>
2022-07-19 22:40:47 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3764,21 +3764,32 @@
         menuBurger.classList.toggle("_burger-active");
         if (menuBody) menuBody.classList.toggle("_active-menu");
     }));
-    let body = document.querySelector("body");
-    if (isMobile.any()) {
-        body.classList.add("touch");
-        let arrow = document.querySelectorAll(".arrow");
-        for (i = 0; i < arrow.length; i++) {
-            let thisLink = arrow[i].previousElementSibling;
-            let subMenu = arrow[i].nextElementSibling;
-            let thisArrow = arrow[i];
-            thisLink.classList.add("parent");
-            arrow[i].addEventListener("click", (function() {
-                subMenu.classList.toggle("open");
-                thisArrow.classList.toggle("active");
-            }));
-        }
-    } else body.classList.add("mouse");
+   let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+		let body=document.querySelector('body');
+if(isMobile.any()){
+		body.classList.add('touch');
+		let arrow=document.querySelectorAll('.arrow');
+	for(i=0; i<arrow.length; i++){
+			let thisLink=arrow[i].previousElementSibling;
+			let subMenu=arrow[i].nextElementSibling;
+			let thisArrow=arrow[i];
+
+			thisLink.classList.add('parent');
+		arrow[i].addEventListener('click', function(){
+			subMenu.classList.toggle('open');
+			thisArrow.classList.toggle('active');
+		});
+	}
+}else{
+	body.classList.add('mouse');
+}
     window["FLS"] = true;
     isWebp();
     menuInit();
